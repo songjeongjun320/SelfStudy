3f3c73c406401fa23f8691aabf39791ee21b0b2a
songjeongjun320 <songjeongjun320@gmail.com>
2022-07-21 14:16:52 -0700

    3sum

--- threesum.py
+++ threesum.py
@@ -44,3 +44,42 @@ for i in range(len(nums)):
                     sum = sum - nums[k]
             sum = sum - nums[j]
 print(output)
+
+"""
+More Fastest version
+
+nums = [14,4,6,-1,10,9,-8,7,-13,14,-13,-11,-8,-9,11,14,-8,-14,-13,7,-10,-15,-13,-11,-11,11,14,13,2,-14,1,-7,-2,14,-1,-15,9,7,-1,3,6,1,7,5,-1,-5,4,-2,-4,-1,-9,-7,-1,-7,-11,3,12,10,-7,-1,12,1,8,-13,1,14,9,-13,6,-7,-3,-11,2,-11,10,-14,-1,-9,0,2,5,6,3,-11,6,7,0,3,3,0,-12,-8,-13,3,-14,-5,2,10,-11,-14,-12,1,-10,5,5,7,-1,11,14,6,-10,-4,-3,8,-7,10,1,8,-1,-11,-15,-6,-12,-13,12,-11]
+# nums = [-1,0,1,2,-1,-4]
+# nums = [0,1,1]
+
+nums.sort()
+sum = 0 # 삼중 포문 안에서 더하는 값을 저장할 변수 / 마지막에는 target이 되는 변수
+output = []
+s = set()
+
+## 제시된 nums의 모든 three sum 경우의 수 판별, 그 중 중복된 sum값 찾아냄.
+for i in range(len(nums)):
+    if i+1 >= len(nums):
+        continue
+    sum = nums[i]
+    j = i+1
+    k = len(nums)-1
+    while j < k:
+        sum = nums[i] + nums[j] + nums[k]
+        if sum == 0:
+            tmp = []
+            tmp.append(nums[i])
+            tmp.append(nums[j])
+            tmp.append(nums[k])
+            tmp.sort()
+            if tuple(tmp) not in s:
+                s.add(tuple(tmp))
+                output.append(tmp)
+            j += 1
+        elif sum < 0:
+            j += 1
+        elif sum > 0:
+            k -= 1   
+
+print(output)
+"""
