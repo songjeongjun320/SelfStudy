0190833d1a2d343516ee21fa765cf317956e317b
songjeongjun320 <songjeongjun320@gmail.com>
2022-07-25 16:46:32 -0700

    07/25/2022

--- Roman_to_Integer.py
+++ Roman_to_Integer.py
@@ -1,6 +1,7 @@
-input = "LVIII"
+input = "MCMXCIV"
 
 roman_to_int = []
+output = 0
 
 for _ in range(len(input)):
     if input[_] == 'I':
@@ -19,5 +20,14 @@ for _ in range(len(input)):
         roman_to_int.append(1000)
 
 roman_to_int.append(0)
+_ = 0
 
-print(roman_to_int)
+while _ < len(roman_to_int) - 1:
+    if roman_to_int[_] >= roman_to_int[_+1]:
+        output += roman_to_int[_]
+    elif roman_to_int[_] < roman_to_int[_+1]:
+        output = output + roman_to_int[_+1] - roman_to_int[_]
+        _ += 1
+    _ += 1
+
+print(output)
