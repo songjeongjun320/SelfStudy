c52c8074dcdc680d610f23a727d7600ef43c497a
songjeongjun320 <songjeongjun320@gmail.com>
2022-08-01 10:30:40 -0700

    08/01/2022

--- Longest_Substring_Without_Repeating_Characters.py
+++ Longest_Substring_Without_Repeating_Characters.py
@@ -3,27 +3,47 @@ Given a astring s, find the length of the longest
 substring without repeating characters
 """
 
+# Time Limitation
+
+# class Solution(object):
+#     def lengthOfLongestSubstring(self,s):
+#         if not s:
+#             return 0
+#         count = 1
+#         for _ in range(len(s)):
+#             tmp_list = []
+#             for j in range(_,len(s)):
+#                 if not s[j] in tmp_list:
+#                     tmp_list.append(s[j])
+#                 else:
+#                     count = max(count,len(tmp_list))
+#                     tmp_list = []
+#             count = max(count,len(tmp_list))
+
+#         return count
+
 class Solution(object):
     def lengthOfLongestSubstring(self,s):
-        if not s:
-            return 0
-        count = 1
-        tmp_list = []
-        for j in range(len(s)):
-            if not s[j] in tmp_list:
-                tmp_list.append(s[j])
+        if s == " ":
+            return 1
+        output_list = []
+        output = 0
+        l, r = 0, 0
+        while r < len(s):
+            if s[r] not in output_list:
+                output_list.append(s[r])
+                output = max(output, r - l + 1)
             else:
-                count = max(count,len(tmp_list))
-                tmp_list = []
-                j = s.index(s[j])+1
-            count = max(count,len(tmp_list))
+                output = max(output, r - l)
+                output_list.clear()
+                l = l + 1
+                r = l
+                continue
+            r += 1                    
+                              
+        return output        
 
-        return count
-                    
-        
-        
-                
 testcase = Solution()
 
-input = "ababcb"
+input = "abcabcbb"
 print(testcase.lengthOfLongestSubstring(input))
