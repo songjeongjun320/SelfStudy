3a240b5bec8c1d1ae28ec3c535c36778f880870f
songjeongjun320 <songjeongjun320@gmail.com>
2022-08-01 16:04:53 -0700

    08/01/2022

--- Longest_Substring_Without_Repeating_Characters.py
+++ Longest_Substring_Without_Repeating_Characters.py
@@ -39,7 +39,7 @@ class Solution(object):
                 l = l + 1
                 r = l
                 continue
-            r += 1                    
+            r += 1
                               
         return output        
 
---
+++ searchInsert.py
@@ -0,0 +1,21 @@
+"""
+Given a sorted array of distinct integers and a target value,
+return the index if the target is found. If not, return the index
+where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+"""
+
+class Solution():
+    def searchInsert(self, nums, target):
+        for _ in range(len(nums)):
+            if target <= nums[_]:
+                return _
+        return len(nums)
+    
+testcase = Solution()
+
+target = 7
+nums = [1,3,5,6]
+print(testcase.searchInsert(nums,target))
